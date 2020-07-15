adbb3ce82eacd561e629c853fbc2dc42f9023a7d
Chris Kuchin <chris.kuchin@neuro-id.com>
2020-07-15 13:23:31 -0600

    update to only collect billing info hourly

--- warehouse.go
+++ warehouse.go
@@ -33,17 +33,28 @@ var (
 )
 
 type warehouseBilling struct {
-	Warehouse          string  `db:"WAREHOUSE_NAME"`
-	CreditsUsed        float64 `db:"CREDITS_USED"`
-	CreditsUsedCompute float64 `db:"CREDITS_USED_COMPUTE"`
-	CreditsUsedCloud   float64 `db:"CREDITS_USED_CLOUD_SERVICES"`
+	Warehouse          string    `db:"WAREHOUSE_NAME"`
+	CreditsUsed        float64   `db:"CREDITS_USED"`
+	CreditsUsedCompute float64   `db:"CREDITS_USED_COMPUTE"`
+	CreditsUsedCloud   float64   `db:"CREDITS_USED_CLOUD_SERVICES"`
+	StartTime          time.Time `db:"START_TIME"`
+	EndTime            time.Time `db:"END_TIME"`
 }
 
 // GatherWarehouseUsageMetrics collects various warehouse metering metrics
 func GatherWarehouseUsageMetrics(db *sql.DB, start chan time.Time, done chan bool) {
 	for rangeStart := range start {
 		if !dry {
-			query := fmt.Sprintf("select * from table(information_schema.warehouse_metering_history(DATE_RANGE_START => to_timestamp_ltz('%s'), DATE_RANGE_END => current_timestamp()));", rangeStart.Format(time.RFC3339))
+			/// calculate first tick after the hour
+			if float64(rangeStart.Minute()) > interval.Minutes() {
+				log.Debug("[WarehouseUsage] Not first tick after the hour, skipping collection")
+				continue
+			}
+
+			end := rangeStart.Truncate(1 * time.Hour)
+			start := end.Add(-1 * time.Hour)
+
+			query := fmt.Sprintf("select * from table(information_schema.warehouse_metering_history(DATE_RANGE_START => to_timestamp_ltz('%s'), DATE_RANGE_END => to_timestamp_ltz('%s')));", start.Format(time.RFC3339), end.Format(time.RFC3339))
 			log.Debugf("[WarehouseUsage] Query: %s", query)
 			rows, err := runQuery(query, db)
 			if err != nil {
@@ -58,11 +69,15 @@ func GatherWarehouseUsageMetrics(db *sql.DB, start chan time.Time, done chan boo
 			for rows.Next() {
 				rows.StructScan(warehouse)
 
-				log.Debugf("[WarehouseUsage] row: %+v", warehouse)
+				if warehouse.StartTime.Equal(start) {
+					log.Debugf("[WarehouseUsage] row: %+v", warehouse)
 
-				warehouseCloudCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsedCloud)
-				warehouseComputeCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsedCompute)
-				warehouseTotalCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsed)
+					warehouseCloudCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsedCloud)
+					warehouseComputeCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsedCompute)
+					warehouseTotalCreditsUsed.WithLabelValues(warehouse.Warehouse).Add(warehouse.CreditsUsed)
+				} else {
+					log.Debugf("[WarehouseUsage] skipped row: %+v")
+				}
 			}
 
 			rows.Close()
